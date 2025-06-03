97a31eaaff466af75d6749ddecf6dfc2e3c03b4d
MThuyet <mthuyet294@gmail.com>
2025-06-03 20:03:50 +0700

    config(api-backend): init api test from laravel 11

--- laravel-backend/database/seeders/DatabaseSeeder.php
+++ laravel-backend/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::insert(
+            [
+                [
+                    'name' => 'John Doe',
+                    'email' => 'john@doe.com',
+                    'password' => bcrypt('password'),
+                ],
+                [
+                    'name' => 'Jane Doe',
+                    'email' => 'jane@doe.com',
+                    'password' => bcrypt('password'),
+                ],
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            ]
+        );
     }
 }
